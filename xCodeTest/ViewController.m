bc13f7d9c7669c45ea0a7c7fe52d4f565dd08d52
namanvaishnav <namanmvaishnav@gmail.com>
2016-08-02 11:30:41 +0530

    NamansMethod Banavi Apade

--- xCodeTest/ViewController.m
+++ xCodeTest/ViewController.m
@@ -19,6 +19,12 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void) namanMethod{
+
+
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
